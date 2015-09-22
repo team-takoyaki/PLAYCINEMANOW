aade3d15453f4a9e8566fa48d0c287b552ab72b0
cy-takashi-honda <takashi_honda@cybird.co.jp>
2015-09-22 16:45:08 +0900

    cronタスク作った

--- playcinemanow.js
+++ playcinemanow.js
@@ -1,7 +1,19 @@
 Cinema = new Mongo.Collection("cinema");
 
 if (Meteor.isServer) {
-  //ここにServer用
+  SyncedCron.add({
+    name: 'Get CinemaObject by cron',
+    schedule: function(parser) {
+      // ==== for debug ====
+      return parser.text('every 3 seconds');
+      // return parser.text('at 10:15 am');
+    },
+    job: function() {
+      var cinemaCronJob = CinemaCronJob();
+      return cinemaCronJob;
+    }
+  });
+  SyncedCron.start();
 }
 
 if (Meteor.isClient) {
@@ -11,4 +23,29 @@ if (Meteor.isClient) {
       return Cinema.find({});
     }
   });
+}
+
+function CinemaCronJob() {
+  var getCinemaInfo = function () {
+    // ここに情報持ってくる処理
+    var cinemaObject = {
+      "title": "xxx",
+      "info": {
+        "trailer_url": "http://xxx"
+      }
+    };
+    return cinemaObject;
+  };
+
+  var insertMongo = function(cinemaInfo) {
+    var now = new Date();
+    var unixTime = Math.floor(now / 1000);
+    Cinema.insert({
+      "title": cinemaInfo["title"],
+      "info": cinemaInfo["info"],
+      "register_date":unixTime
+    });
+  };
+
+  insertMongo(getCinemaInfo());
 }
