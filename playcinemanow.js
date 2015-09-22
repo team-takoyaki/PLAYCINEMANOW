0e48fca102d82f8abc0e5eaf1caab7ef05c363c5
t-kashima <takumi.kashima@nanapi.co.jp>
2015-09-22 18:50:06 +0900

    YouTubeのURLを取得するようにしました

--- playcinemanow.js
+++ playcinemanow.js
@@ -1,80 +1,92 @@
 Cinema = new Mongo.Collection("cinema");
 
-
 if (Meteor.isServer) {
-  HTTP.get(
-    'http://movies.yahoo.co.jp/trailer/intheater/',
-    function(error, result){
-      console.log(result);
-    }
-  );
-  SyncedCron.add({
-    name: 'Get CinemaObject by cron',
-    schedule: function(parser) {
-      // ==== for debug ====
-      return parser.text('every 3 seconds');
-      // return parser.text('at 10:15 am');
-    },
-    job: function() {
-      var cinemaCronJob = CinemaCronJob();
-      return cinemaCronJob;
-    }
-  });
-    // SyncedCron.start();
-var videoSearch = function(keyword, count, callback) {
-    var YouTube = Meteor.npmRequire('youtube-node');
-    var youTube = new YouTube();
-    youTube.setKey('AIzaSyCAkVyShgocmoAJYYbnSd1Xm-Jmce-aCs4');
-    youTube.search(keyword, count, function(error, result) {
-        if (error) {
-            console.log(error);
-        } else {
-            var count = result.items.length;
-            if (0 >= count) {
-                return;
-            }
-            var item = result.items[0];
-            var videoId = item.id.videoId;
-            console.log('VideoID: ' + videoId);
-            if (callback != null) {
-                callback();
-            }
+    HTTP.get(
+        'http://movies.yahoo.co.jp/trailer/intheater/',
+        function(error, result){
+            console.log(result);
+        }
+    );
+    SyncedCron.add({
+        name: 'Get CinemaObject by cron',
+        schedule: function(parser) {
+            // ==== for debug ====
+            return parser.text('every 3 seconds');
+            // return parser.text('at 10:15 am');
+        },
+        job: function() {
+            var cinemaCronJob = CinemaCronJob();
+            return cinemaCronJob;
         }
     });
-}
-    videoSearch('abc', 1, null);
+    // SyncedCron.start();
+
+    videoSearch('ピクセル　プロモーション映像　劇場予告編1');
 }
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    cinema_list: function () {
-      return Cinema.find({});
-    }
-  });
+    // This code only runs on the client
+    Template.body.helpers({
+        cinema_list: function () {
+            return Cinema.find({});
+        }
+    });
 }
 
 function CinemaCronJob() {
-  var getCinemaInfo = function () {
-    // ここに情報持ってくる処理
-    var cinemaObject = {
-      "title": "xxx",
-      "info": {
-        "trailer_url": "http://xxx"
-      }
+    var getCinemaInfo = function () {
+        // ここに情報持ってくる処理
+        var cinemaObject = {
+            "title": "xxx",
+            "info": {
+                "trailer_url": "http://xxx"
+            }
+        };
+        return cinemaObject;
     };
-    return cinemaObject;
-  };
 
-  var insertMongo = function(cinemaInfo) {
-    var now = new Date();
-    var unixTime = Math.floor(now / 1000);
-    Cinema.insert({
-      "title": cinemaInfo["title"],
-      "info": cinemaInfo["info"],
-      "register_date":unixTime
+    var insertMongo = function(cinemaInfo) {
+        var now = new Date();
+        var unixTime = Math.floor(now / 1000);
+        Cinema.insert({
+            "title": cinemaInfo["title"],
+            "info": cinemaInfo["info"],
+            "register_date":unixTime
+        });
+    };
+
+    // insertMongo(getCinemaInfo());
+}
+
+function videoSearch(keyword) {
+    var YouTube = Meteor.npmRequire('youtube-node');
+    var youTube = new YouTube();
+    youTube.setKey('AIzaSyCAkVyShgocmoAJYYbnSd1Xm-Jmce-aCs4');
+    youTube.search(keyword, 1, function(error, result) {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        var count = result.items.length;
+        if (0 >= count) {
+            return;
+        }
+        var item = result.items[0];
+        var videoId = item.id.videoId;
+        console.log('VideoID: ' + videoId);
+        getYouTubeInfo(videoId);
     });
-  };
+}
 
-  // insertMongo(getCinemaInfo());
+function getYouTubeInfo(videoId) {
+    var youTubeDownloader = Meteor.npmRequire('youtube-dl');
+    var url = 'https://www.youtube.com/watch?v=' + videoId;
+    var options = [];
+    youTubeDownloader.getInfo(url, options, function(error, info) {
+        if (error) {
+            return;
+        }
+        // ここでデータベースに入れる
+        console.log('url: ', info.url);
+    });
 }
