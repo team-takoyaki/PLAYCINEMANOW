5d4de2b411d5d78b51a36a3b7e884fc54178c713
cy-takashi-honda <takashi_honda@cybird.co.jp>
2015-09-22 19:57:05 +0900

    一旦突っ込む

--- playcinemanow.js
+++ playcinemanow.js
@@ -1,17 +1,11 @@
 Cinema = new Mongo.Collection("cinema");
 
-var client =  Meteor.npmRequire('cheerio-httpcli');
-client.fetch('http://movies.yahoo.co.jp/trailer/intheater/',
-  {},
-  parseCinemaTitles
-);
-
 if (Meteor.isServer) {
   SyncedCron.add({
-    name: 'Get CinemaObject by cron',
+    name: 'CinemaInfo cron job',
     schedule: function(parser) {
       // ==== for debug ====
-      return parser.text('every 3 seconds');
+      return parser.text('every 10 seconds');
       // return parser.text('at 10:15 am');
     },
     job: function() {
@@ -19,9 +13,7 @@ if (Meteor.isServer) {
       return cinemaCronJob;
     }
   });
-    // SyncedCron.start();
-    
-    videoSearch('ピクセル　プロモーション映像　劇場予告編1');
+  SyncedCron.start();
 }
 
 if (Meteor.isClient) {
@@ -33,39 +25,47 @@ if (Meteor.isClient) {
     });
 }
 
-function parseCinemaTitles(err, $, result) {
-  var cinemaTitles = [];
-  $('li.col a').each(function() {
-    if ($(this).attr('title')) {
-      cinemaTitles.push($(this).attr('title'));
-    }
-  });
-  // console.log(cinemaTitles);
+function CinemaCronJob() {
+    var getCinemaInfo = function (err, $, res) {
+      var cinemaTitles = [];
+      $('li.col a').each(function() {
+        if ($(this).attr('title')) {
+          cinemaTitles.push($(this).attr('title'));
+        }
+      });
+
+      for (var i = 0; i < cinemaTitles.length; i++) {
+        videoSearch(cinemaTitles[i]);
+      }
+    };
+
+    var client =  Meteor.npmRequire('cheerio-httpcli');
+    client.fetch('http://movies.yahoo.co.jp/trailer/intheater/',
+      {},
+      getCinemaInfo
+    );
 }
 
-function CinemaCronJob() {
-    var getCinemaInfo = function () {
-        // ここに情報持ってくる処理
-        var cinemaObject = {
-            "title": "xxx",
-            "info": {
-                "trailer_url": "http://xxx"
-            }
-        };
-        return cinemaObject;
-    };
+function operateMongo(cinemaInfo) {
 
-    var insertMongo = function(cinemaInfo) {
-        var now = new Date();
-        var unixTime = Math.floor(now / 1000);
-        Cinema.insert({
-            "title": cinemaInfo["title"],
-            "info": cinemaInfo["info"],
-            "register_date":unixTime
-        });
+  var parseForMongo = function(info) {
+    return {
+      "title": info["title"],
+      "info": {
+        "trailer_url": info["url"],
+        "description": info["description"]
+      }
     };
+  };
 
-    // insertMongo(getCinemaInfo());
+  var insertMongo = function(insertObject) {
+    var now = new Date();
+    var unixTime = Math.floor(now / 1000);
+    insertObject["register_date"] = unixTime;
+    // Cinema.insert(insertObject);
+  };
+
+  insertMongo(parseForMongo(cinemaInfo));
 }
 
 function videoSearch(keyword) {
@@ -84,6 +84,9 @@ function videoSearch(keyword) {
         var item = result.items[0];
         var videoId = item.id.videoId;
         console.log('VideoID: ' + videoId);
+        if (!videoId) {
+          return;
+        }
         getYouTubeInfo(videoId);
     });
 }
@@ -93,10 +96,13 @@ function getYouTubeInfo(videoId) {
     var url = 'https://www.youtube.com/watch?v=' + videoId;
     var options = [];
     youTubeDownloader.getInfo(url, options, function(error, info) {
-        if (error) {
-            return;
-        }
-        // ここでデータベースに入れる
-        console.log('url: ', info.url);
+      if (error) {
+        return;
+      }
+      if (!info) {
+        return;
+      }
+
+      operateMongo(info);
     });
 }
